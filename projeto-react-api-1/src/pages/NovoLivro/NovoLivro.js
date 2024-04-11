75676f6dd37be404b98011861aa6fa382b0724cf
sr-paixao <henriquemfpaixao@gmail.com>
2024-04-11 20:41:34 -0300

    insert de dados

--- projeto-react-api-1/src/pages/NovoLivro/NovoLivro.js
+++ projeto-react-api-1/src/pages/NovoLivro/NovoLivro.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import Input from '../../components/Input/Input'
 import Select from '../../components/Select/Select'
@@ -6,40 +7,83 @@ import Select from '../../components/Select/Select'
 import styles from './NovoLivro.module.css'
 
 export default function NovoLivro() {
+  const navigate = useNavigate();
+
+
+
   const [categories, setCategories] = useState([])
+  const [book, setBook] = useState({})
 
 
-
-useEffect(()=>{
-  fetch('http://localhost:5000/categories', {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'aplication/json',
-    },
-  })
-
-    .then((response) =>response.json())
-    .then(
-      (data)=>{setCategories(data)
-      console.log(data)
-      }
-    )
-
-    .catch((error) => {
-      console.log(error)
+  useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'aplication/json',
+      },
     })
-},[])
+      .then((response) => response.json())
+      .then((data) => {
+        setCategories(data)
+        console.log(data)
+      })
+
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  function handleChangeBook(event) {
+    setBook({ ...book, [event.target.name]: event.target.value })
+    console.log(book)
+  }
+
+  function handleChangeCategory(event) {
+    setBook({
+      ...book,
+      categories: {
+        id: event.target.value,
+        categories: event.target.options[event.target.selectedIndex].text,
+      },
+    })
+    console.log(book)
+  }
+
+    function createBook(book) {
+      fetch('http://localhost:5000/books', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'aplication/json',
+        },
+        body: JSON.stringify(book),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log(data)
+          navigate('/livros')
+        })
+        .catch((error) => {
+          console.log(error)
+        })
+    }
+
+    function submit(event) {
+      event.preventDefault()
+      createBook(book)
+      
+    }
   return (
     <section className={styles.novolivros_container}>
       <h1>Cadastre livro</h1>
 
-      <form>
+      <form onSubmit={submit}>
         <Input
           type="text"
           name="nome_livro"
           id="nome_livro"
           placeholder="Digite o titulo do livro"
           text="Digite o titulo do livro"
+          handlerOnChange={handleChangeBook}
         />
 
         <Input
@@ -48,6 +92,7 @@ useEffect(()=>{
           id="nome_autor"
           placeholder="Digite o nome do autor"
           text="Digite o nome do autor"
+          handlerOnChange={handleChangeBook}
         />
 
         <Input
@@ -56,22 +101,15 @@ useEffect(()=>{
           id="descricao_livro"
           placeholder="Digite a descricao do livro"
           text="digite a descricao do livro"
+          handlerOnChange={handleChangeBook}
         />
 
-        <Select name="categoria-id" text="selecione a categoria do livro" options={categories}/>
-
-        {/* <p>
-          <input type="text" placeholder="Nome do livro" id="" />
-        </p>
-
-        <p>
-          <input type="text" placeholder="Nome do autor" id="" />
-        </p>
-
-        <p>
-          <input type="text" placeholder="Descriçao do livro" id="" />
-        </p> */}
-
+        <Select
+          name="categoria-id"
+          text="selecione a categoria do livro"
+          options={categories}
+          handlerOnChange={handleChangeCategory}
+        />
         <p>
           <button type="submit">Enviar</button>
         </p>
